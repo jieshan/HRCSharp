5d6faaa5b83257fd6e1749404cb71cc816076710
jieshan <jieshan22@gmail.com>
2015-11-12 05:44:39 +0000

    Algorithm-Sorting: Intro to Tutorial Challenges

---
+++ IntroToTutorialChallenges/IntroToTutorialChallenges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace IntroToTutorialChallenges
+{
+    class IntroToTutorialChallenges
+    {
+        static void Main(String[] args)
+        {
+            int value = int.Parse(Console.ReadLine());
+            int lower = 0;
+            int upper = int.Parse(Console.ReadLine());
+            string[] values = Console.ReadLine().Split(' ');
+            int pointer = (lower + upper) / 2;
+            while (lower < upper) 
+            {
+                if (int.Parse(values[pointer]) < value)
+                {
+                    lower = pointer;
+                }
+                else if (int.Parse(values[pointer]) > value)
+                {
+                    upper = pointer;
+                }
+                else 
+                {
+                    Console.WriteLine(pointer);
+                    return;
+                }
+                pointer = (lower + upper) / 2;
+            }
+            Console.WriteLine(pointer);
+        }
+    }
+}
