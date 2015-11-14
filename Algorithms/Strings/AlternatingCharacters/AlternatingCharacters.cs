a42ee661f19f03c522700c568e0b079e4579b6a9
jieshan <jieshan22@gmail.com>
2015-11-14 05:12:59 +0000

    AlternatingCharacters

---
+++ Algorithms/Strings/AlternatingCharacters/AlternatingCharacters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternatingCharacters
+{
+    class AlternatingCharacters
+    {
+        static void Main(String[] args)
+        {
+            int numCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numCases; i++)
+            {
+                string input = Console.ReadLine();
+                int count = 0;
+                int prev = 0;
+                int current = 1;
+                while (current < input.Length)
+                {
+                    if (input[prev] == input[current])
+                    {
+                        count++;
+                        current++;
+                    }
+                    else
+                    {
+                        prev = current;
+                        current++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
