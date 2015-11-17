3160a72c53cf3a7e18f8c17871a9a90b6b3bc147
jieshan <jieshan22@gmail.com>
2015-11-17 08:23:54 +0000

    ClosestNumbers

---
+++ Algorithms/Sorting/ClosestNumbers/ClosestNumbers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosestNumbers
+{
+    class ClosestNumbers
+    {
+        static void Main(String[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<long> list = new List<long>();
+            string[] input = Console.ReadLine().Split(' ');
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(long.Parse(input[i]));
+            }
+            list.Sort();
+            long diff = list.ElementAt(count - 1) - list.ElementAt(0);
+            string output = "";
+            for (int i = 0; i < count - 1; i++)
+            {
+                if (list.ElementAt(i + 1) - list.ElementAt(i) < diff)
+                {
+                    output = list.ElementAt(i) + " " + list.ElementAt(i + 1);
+                    diff = list.ElementAt(i + 1) - list.ElementAt(i);
+                }
+                else if (list.ElementAt(i + 1) - list.ElementAt(i) == diff)
+                {
+                    output += " " + list.ElementAt(i) + " " + list.ElementAt(i + 1);
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
