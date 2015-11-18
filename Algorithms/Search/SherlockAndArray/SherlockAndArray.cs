157bcf14c0d0f85fd60fb1840ee8ace35a8b4b93
jieshan <jieshan22@gmail.com>
2015-11-18 09:36:02 +0000

    SherlockAndArray

---
+++ Algorithms/Search/SherlockAndArray/SherlockAndArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SherlockAndArray
+{
+    class SherlockAndArray
+    {
+        static void Main(String[] args)
+        {
+            int numCases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numCases; i++)
+            {
+                int len = int.Parse(Console.ReadLine());
+                string[] input = Console.ReadLine().Split(' ');
+                long sum = 0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    sum += int.Parse(input[j]);
+                }
+                long left = 0;
+                long right = sum;
+                bool found = false;
+                for (int j = 0; j < input.Length; j++) 
+                {
+                    int current = int.Parse(input[j]);
+                    right -= current;
+                    if (left == right)
+                    {
+                        found = true;
+                        break;
+                    }
+                    else
+                    {
+                        left += current;
+                    }
+                }
+                if (found)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
