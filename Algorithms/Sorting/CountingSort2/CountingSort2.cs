ba3bcc61ccd6beb48415c6c3ea1f37ab9465d597
jieshan <jieshan22@gmail.com>
2015-11-24 05:34:41 +0000

    CountingSort2

---
+++ Algorithms/Sorting/CountingSort2/CountingSort2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountingSort2
+{
+    class CountingSort2
+    {
+        static void Main(String[] args)
+        {
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(3000000)));
+            int num = int.Parse(Console.ReadLine());
+            string[] numbers = Console.ReadLine().Split(' ');
+            int[] count = new int[100];
+            for (int i = 0; i < num; i++)
+            {
+                count[int.Parse(numbers[i])]++;
+            }
+            string output = "";
+            for (int i = 0; i < count.Length; i++)
+            {
+                output += String.Concat(Enumerable.Repeat(i + " ", count[i]));
+            }
+            Console.WriteLine(output.Substring(0, output.Length - 1));
+        }
+    }
+}
